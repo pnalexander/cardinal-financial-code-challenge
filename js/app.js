b4f6d922a6f6ecca045fa2a304f3c81f1168c45c
Paul Alexander <xander360tips@gmail.com>
2020-12-10 14:25:42 -0500

    add vue-router

--- js/app.js
+++ js/app.js
@@ -1,32 +1,86 @@
+// Vue.use(vueRouter);
+
 Vue.component('employee', {
-  props: ['job-title', 'name', 'department','headshot'],
+  props: ['employeeId', 'jobTitle', 'name', 'department','headshot'],
+
+  data() {
+    return {
+      // employeeId: ''
+    }
+  },
 
   template: `
-    <article class="uk-card uk-card-default">
-      <div class="uk-card-media-top">
-          <img :src="headshot" alt="">
-      </div>
-      <div class="uk-card-body">
-          <h3 class="uk-card-title">{{ name }}</h3>
-          <p>{{ job-title }}</p>
-      </div>
+    <article class="uk-card uk-card-default uk-link-toggle uk-width-1-3@m uk-padding-remove">
+        <div class="uk-card-body">
+          <div class="uk-grid-small uk-flex-middle" uk-grid>
+              <div class="uk-width-auto">
+                  <img class="uk-border-circle" width="94" height="94" :src="headshot">
+              </div>
+              <div class="uk-width-expand">
+                  <h3 class="uk-card-title uk-link-heading uk-margin-remove-bottom">{{ name }}</h3>
+                  <p class="uk-text-meta uk-margin-remove-top uk-margin-remove-bottom">{{ jobTitle }}</p>
+                  <p class="uk-text-meta uk-margin-remove-top small">{{ department }} Department</p>
+              </div>
+          </div>
+        </div>
     </article>
+  `,
+
+  methods: {
+    // selectedEmployee(id) {
+    //   // console.log(this.employeeDirectory.find(employee => employee.id == this.key));
+    //   alert(id);
+    // }
+  }
+});
+
+let profile = Vue.component('profile', {
+  // props: [],
+
+  data() {
+    return {
+      // employeeId: ''
+    }
+  },
+
+  template: `
+    <h1>{{ this.$route.params.id}}</h1>
   `
+});
+
+let routes = [
+  {
+    path: '/profile/:id',
+    component: profile
+  }
+];
+
+const router = new VueRouter({
+  routes
 });
 
 
 new Vue({
   el: 'main',
+  router,
   data: {
     employeeDirectory: [],
     searchParam: '',
-    department: ''
+    department: '',
+    searchQuery: '',
+    id: ''
   },
 
   methods: {
     // Define parameters for searchResults() based on user input
-    filterResults(selectedFilters) {
+    filterResults(searchParam, resultFilters) {
+      //
+    },
 
+    selectedEmployee(id) {
+      // console.log(this.employeeDirectory.find(employee => employee.id == this.key));
+      // alert(id);
+      console.log(this.key);
     }
   },
 
@@ -34,9 +88,7 @@ new Vue({
     // Search for results with given parameters
     // Returns array
     searchResults() {
-      // return this.employeeDirectory.filter(employee => employee.name.first == this.searchParam && employee.department == 'Executive');
-      return this.employeeDirectory.filter(employee => employee.name.first == this.searchParam && employee.department == this.department);
-      // alert(this.searchParam);
+      return this.employeeDirectory.filter(employee => (employee.name.first.toLowerCase().includes(this.searchParam.toLowerCase()) || employee.name.last.toLowerCase().includes(this.searchParam.toLowerCase())) && employee.department == this.department);
     }
   },
 
