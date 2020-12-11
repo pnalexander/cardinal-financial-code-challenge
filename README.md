39fc91d3f363aa0802966f0c26dea3b9b675208b
Paul Alexander <xander360tips@gmail.com>
2020-12-11 12:52:13 -0500

    update readme; clean up unused code

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Code challenge for Cardinal Financial interview process
 I decided to use Vue.js, which I am not very familiar with, so I would be further challenged by this exercise. In the end, it turns out I still have yet to fully grasp Vue's data binding and component communication, among other things. This resulted in an incomplete code challenge. Nevertheless, I learned a substantial amount along the way and I'm satisfied with my efforts.
 
 At the time of this edit, it's almost 5am on Friday 12/11/20. I'm going to bed and I'll pick this back up in a few hours to see if I can figure it out. If I can't figure it out before noon, then I'll be submitting an incomplete code challenge. Either way, I fully intend to complete the challenge, regardless of when I submit it.
+
+### UPDATE
+
+I found a solution. It's not what I had hoped for, but it works. Instead of using vue-router and communicating between components, I just pass information as a prop and use it in a single component. I still intend to improve on this in the future! This will have to do because I'm out of time.
--- js/app.js
+++ js/app.js
@@ -1,75 +1,26 @@
-let directory = Vue.component('directory', { // employee info card wrapper. Currently not in use.
-  props: ['searchResults'],
-
-  template: `
-    <div>
-      <employee
-        v-for="(employee, index) in searchResults"
-        :key="employee.id"
-        v-bind:employee-id="employee.id"
-        v-bind:job-title="employee.job_title"
-        v-bind:full-name="employee.name.first + ' ' + employee.name.last"
-        v-bind:first-name="employee.name.first"
-        v-bind:last-name="employee.name.last"
-        v-bind:department="employee.department"
-        v-bind:headshot="employee.headshot.large"
-      ></employee>
-    </div>
-  `
-});
-
-Vue.component('employee', { // employee info card
-  props: ['employeeId', 'jobTitle', 'fullName', 'firstName', 'lastName', 'department','headshot', 'profile'],
-
-  data() {
-    return {
-      //
-    }
-  },
-
-  // template: `
-  //   <article class="col s12 m6 l4">
-  //     <div class="card">
-  //       <div class="card-content">
-  //         <router-link :to="/profile/ + employeeId">
-  //           <span v-on:hideDirectory="showProfile">
-  //           <img :src="headshot">
-  //             <h4 class="hide-on-small-only truncate">{{ fullName }}</h4>
-  //             <h4 class="hide-on-med-and-up truncate">{{ firstName }}<br>{{ lastName }}</h4>
-  //           </span>
-  //         </router-link>
-  //       </div>
-  //       <div class="card-action">
-  //         <p class="truncate medium">{{ jobTitle }}</p>
-  //         <p class="truncate small">{{ department }} Department</p>
-  //       </div>
-  //       <div class="card-reveal">
-  //         <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-  //         <p>Here is some more information about this product that is only revealed once clicked on.</p>
-  //       </div>
-  //     </div>
-  //   </article>
-  // `,
+Vue.component('employee', { // employee profile card
+  props: ['profile'],
 
   template: `
     <article class="col s12 m6 l4">
       <div class="card">
         <div class="card-content">
-            <a href="#"><img :src="headshot" class="activator"></a>
-            <a href="#"><h4 class="hide-on-small-only truncate activator">{{ fullName }}</h4></a>
-            <a href="#"><h4 class="hide-on-med-and-up truncate activator">{{ firstName }}<br>{{ lastName }}</h4></a>
+            <a href="#"><img :src="profile.headshot.large" class="activator"></a>
+            <a href="#"><h4 class="hide-on-small-only truncate activator">{{ profile.name.first + ' ' + profile.name.last }}</h4></a>
+            <a href="#"><h4 class="hide-on-med-and-up truncate activator">{{ profile.name.first }}<br>{{ profile.name.last }}</h4></a>
         </div>
         <div class="card-action">
-          <p class="truncate medium">{{ jobTitle }}</p>
-          <p class="truncate small">{{ department }} Department</p>
+          <p class="truncate medium">{{ profile.job_title }}</p>
+          <p class="truncate small">{{ profile.department }} Department</p>
         </div>
         <div class="card-reveal">
           <span class="card-title">{{ profile.name.first + ' ' + profile.name.last }}<i class="material-icons right">close</i></span>
-          <p class="truncate medium">Job Title: {{ jobTitle }}</p>
-          <p class="truncate small">Department: {{ department }}</p>
+          <p class="truncate medium">Job Title: {{ profile.job_title }}</p>
+          <p class="truncate small">Department: {{ profile.department }}</p>
           <p class="smallest">Phone: {{ profile.phone }}</p>
           <p class="smallest">Email: {{ profile.email }}</p>
           <p class="smallest">Hire Date: {{ profile.date_started }}</p>
+          <p class="smallest">Location: {{ profile.location.city + ', ' + profile.location.state }}</p>
           <p class="small skill-heading">Skills:</p>
           <ul class="skills">
             <li v-for="skill in profile.skills">{{ skill.id }}</li>
@@ -77,78 +28,17 @@ Vue.component('employee', { // employee info card
         </div>
       </div>
     </article>
-  `,
-
-  methods: {
-    showProfile() { // Should emit event that root element can listen for and update isHidden value. Currently not working.
-      this.$emit('hide-directory');
-    }
-  }
+  `
 });
-
-let profile = Vue.component('profile', { // Employee profile. Presents further employee info once their card is selected. Works, but not as originally intended.
-  props: ['profileId'],
-
-  data() {
-    return {
-      profile: {}
-    }
-  },
-
-  methods: { // Find the individual employee based on their id (passed via routes)
-    getProfile() {
-      axios.get('https://raw.githubusercontent.com/pnalexander/cardinal-financial-code-challenge/v1/directory.json').then(response => this.profile = response.data.results.find(profile => profile.id == this.$route.params.id));
-    }
-  },
-
-  // Starter to verify that data is passed correctly. Eventually, the entire employee profile will live here
-  template: `
-
-  <article>
-    <h2>{{ this.profile.name.first + ' ' + this.profile.name.last }}</h2>
-    <h5>Job Title: {{ this.profile.job_title }}</h5>
-    <h6>Department: {{ this.profile.department }}</h6>
-  </article>
-  `,
-
-  created() { // get the selected employee profile
-    this.getProfile();
-  }
-});
-
-let routes = [
-  { // returns default view. Not currently in use.
-    path: '/',
-    component: directory
-  },
-  { // returns profile view
-    path: '/profile/:id',
-    component: profile
-  },
-
-];
-
-const router = new VueRouter({
-  routes
-});
-
 
 new Vue({ // root Vue instance
   el: 'main',
-  router,
   data: {
     employeeDirectory: [],
     searchParam: '',
     department: '',
     searchQuery: '',
     id: '',
-    isHidden: false
-  },
-
-  methods: {
-    directoryHide() { // should fire when an employee name is clicked. Currently not working. Data binding or component communication issue?
-      isHidden: true;
-    }
   },
 
   computed: {
